400a962fb570fc52004e5c99e1098c304ca2a3a6
XnoahR <rio.ae23@gmail.com>
2022-12-30 21:27:23 +0700

    Update Spawner + Test Coroutine

--- TowerDefence/Assets/Spawner.cs
+++ TowerDefence/Assets/Spawner.cs
@@ -7,22 +7,31 @@ public class Spawner : MonoBehaviour
     public bool delay = false;
     public GameObject Enemy;
     public int limiter = 1;
+    public bool WaveStarted =false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        WaveStarted = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(WaveStarted){
+            Debug.Log("Wave Start");
+            StartCoroutine(WaveAttack());
         if(delay == false && limiter <= 10){
                 StartCoroutine(spawn());
+        }
         }
         
     }
 
+    IEnumerator WaveAttack(){
+        yield return new WaitForSeconds(15);
+        WaveStarted = false;
+        Debug.Log("Wave End");
+    }
     IEnumerator spawn(){
         Instantiate(Enemy,transform.position,transform.rotation);
         delay = true;
