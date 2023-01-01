fd05e5044f0d6c62c81844237d350c0c5d5b0852
XnoahR <rio.ae23@gmail.com>
2023-01-01 17:03:33 +0700

    new

--- TowerDefence/Assets/BulletScript.cs
+++ TowerDefence/Assets/BulletScript.cs
@@ -15,27 +15,20 @@ public Enemy enemyTarget;
     void Start()
     {
         rb2 = GetComponent<Rigidbody2D>();
-        
+        transform.position = Vector2.MoveTowards(transform.position,tScript.CurrentEnemyTarget.transform.position,bulletSpeed);
+        Destroy(gameObject,3f);
     }
 
     void moveBullet(){
-        transform.position = Vector2.MoveTowards(transform.position,tScript.CurrentEnemyTarget.transform.position,bulletSpeed*Time.deltaTime);
+        //transform.position = Vector2.MoveTowards(transform.position,tScript.CurrentEnemyTarget.transform.position,bulletSpeed*Time.deltaTime);
 
     }
     // Update is called once per frame
 
-    private void findEnemy(){
-        tScript = GameObject.FindGameObjectWithTag("Enemy").GetComponent<TurretScript>();
-    }
+   
     void Update()
     {
-    findEnemy();
-    if(tScript.enemylist == null){
-        return;
-    }
-    else{
-     moveBullet();   
-    }
+   
     }
     
 }
